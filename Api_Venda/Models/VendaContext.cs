220d0d78149504ca4aa0f001aa6f0692ad1c47b0
marciaandrea <marciaandreacb@outlook.com>
2018-03-20 09:18:38 -0300

    Adicionando na pasta Models a Classe VendaContext

---
+++ Api_Venda/Models/VendaContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Venda.Models
+{
+    public class VendaContext
+    {
+    }
+}
